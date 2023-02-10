dab06f8e782a069004077626e4781357e36b1787
archit0712 <57456294+archit0712@users.noreply.github.com>
2023-02-10 02:42:40 -0500

    error handling invalid file name

--- ftpClient.py
+++ ftpClient.py
@@ -8,14 +8,16 @@ if __name__ == "__main__":
             server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             if connection_string is None :
                 connection_string = input("Enter Command and port number to connect:")
+                print('to terminate the program enter "exit" ')
+                print('to download enter get <filename>')
+                print('for uploading enter upload <filename>')
             data = connection_string.split()
             port = int(data[1])
             server.connect((ip,port))
-            print('to terminate the program enter "exit" ')
-            print('to download enter get <filename>')
-            print('for uploading enter upload <filename>')
+            
         except :
             print("Please try again with correct port or command!")
+            break
         else : 
             data = input("Enter command:")
             command = data.split()
@@ -23,23 +25,35 @@ if __name__ == "__main__":
             if command[0] == 'get':
                 print('requesting ' + command[1]+ ' from server...')
                 server.send(data.encode('utf-8'))
-                file = open('new'+command[1], 'wb')
-                line = server.recv(1024)
-                while(line):
-                    file.write(line)
+                try :
+                    check_file = open(command[1], 'rb')
+                except :
+                    print('File Not Found Please try again')
+                else :
+                        
+                    file = open('new'+command[1], 'wb')
                     line = server.recv(1024)
-                file.close()
-                print("File received !")
+                    while(line):
+                        file.write(line)
+                        line = server.recv(1024)
+                    file.close()
+                    print("File received !")
             elif command[0] == 'upload' :
-                print('sending '+ command[1]+' to server...')
-                server.send(data.encode('utf-8'))
-                file = open( command[1], 'rb')
-                line = file.read(1024)
-                while(line):
-                    server.send(line)
+                
+                try:
+                    file = open( command[1], 'rb')
+                except:
+                    print('Invalid File Name Please Check')
+                else:
+    
+                    server.send(data.encode('utf-8'))
+                    
                     line = file.read(1024)
-                file.close()
-                print('File Uploaded!')
+                    while(line):
+                        server.send(line)
+                        line = file.read(1024)
+                    file.close()
+                    print('File Uploaded!')
             elif data == 'exit' :
                 server.send(data.encode('utf-8'))
                 print("Program Terminated!")
